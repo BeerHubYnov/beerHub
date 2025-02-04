7fa7c56a3caa66113b093a8a2dbcd114440371a8
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2025-02-04 13:55:18 +0100

    fix position objet 3D

--- src/components/home/animation/Scene.tsx
+++ src/components/home/animation/Scene.tsx
@@ -1,4 +1,4 @@
-import { Html, OrbitControls, useProgress } from "@react-three/drei";
+import { Html, useProgress } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
 import React, { Suspense } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
@@ -11,20 +11,20 @@ const Scene: React.FC = () => {
   const gltf = useLoader(GLTFLoader, "/models/beer.glb");
   return (
     <Suspense fallback={<Loader />}>
-      <Canvas camera={{ position: [-0.5, 0.1, 0.2] }} shadows>
+      <Canvas camera={{ position: [0, 2, 5] }} shadows>
+
         <directionalLight
-          position={[1.3, -2.0, -2.4]}
+          position={[1.3, 2.0, 2.4]}
           castShadow
           intensity={Math.PI * 1}
         />
         <primitive
           object={gltf.scene}
-          position={[0, 1, 0]}
-          scale={[0.01, 0.01, 0.01]}
-          rotation={[8, -6, 7]} 
+          position={[0, 0, 1]}
+          scale={[0.03, 0.03, 0.03]}
+          rotation={[0, 0, 0]} 
           children-0-castShadow
         />
-        <OrbitControls target={[0, 1, 0]} />
       </Canvas>
     </Suspense>
   );
