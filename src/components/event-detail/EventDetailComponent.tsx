f8fea7454949648818a4ef81d934bcd126741cb0
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2025-02-25 11:40:08 +0100

    misc bar list / card

--- src/components/event-detail/EventDetailComponent.tsx
+++ src/components/event-detail/EventDetailComponent.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 interface Event {
   id: string;
+  dateHour: string;
   title: string;
   description: string;
-  dateHour: string;
-
+  id_Bar: string;
+  Bar: {
+    id: string;
+    name: string;
+    description: string;
+    happyHoure: string;
+    localisationX: number;
+    localisationY: number;
+  };
 }
 
 interface EventDetailComponentProps {
@@ -16,9 +25,17 @@ const EventDetailComponent: React.FC<EventDetailComponentProps> = ({ event }) =>
   return (
     <div className="profil">
       <div className="card-event">
-      <h2>{event.title}</h2>
-      <p>{event.description}</p>
-      <p>Happy Hour : {event.dateHour}</p>
+     <h3>{event.title}</h3>
+      <p><strong>Description :</strong> {event.description}</p>
+      <p><strong>Date :</strong> {new Date(event.dateHour).toLocaleString()}</p>
+      <p><strong>Lieu :</strong> {event.Bar.name}</p>
+      <p><strong>Happy Hour :</strong> {event.Bar.happyHoure}</p>
+      <p><strong>Localisation :</strong> {event.Bar.localisationX}, {event.Bar.localisationY}</p>
+      <NavLink to={`/event/${event.id}`}>Voir les détails</NavLink>
+      <br />
+      <NavLink to={`/event-edit/${event.id}`}>Modifier</NavLink>
+      <br />
+      <NavLink to={`/event-delete/${event.id}`}>Supprimer</NavLink>
       </div>
 
      
