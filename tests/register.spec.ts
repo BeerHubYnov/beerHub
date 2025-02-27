56432dee73fa558dda8b4cc7a5427a452202d47e
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2025-02-27 14:15:32 +0100

    clean comment

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -5,7 +5,6 @@ test("La page de formulaire de register doit se charger et vérifier le bon dér
   page,
 }) => {
   await page.goto("/register");
-  // await page.pause();
   const headTitle = page.getByRole("heading", { name: "Inscription" });
   await expect(headTitle).toBeVisible();
   await page.getByTestId('register-username').fill("testPlaywright727");
@@ -14,7 +13,7 @@ test("La page de formulaire de register doit se charger et vérifier le bon dér
   await page.getByTestId('register-password-confirm').fill("123Test");
   await page.getByTestId('register-submit').click();
 
-  // await page.waitForURL('**/login');
+  
 
 });
 
@@ -23,7 +22,6 @@ test("La page de formulaire de register doit se charger et afficher un message d
   page,
 }) => {
   await page.goto("/register");
-  // await page.pause();
   const headTitle = page.getByRole("heading", { name: "Inscription" });
   await expect(headTitle).toBeVisible();
   await page.getByTestId('register-username').fill("testPlaywright772");
@@ -31,7 +29,7 @@ test("La page de formulaire de register doit se charger et afficher un message d
   await page.getByTestId('register-password').fill("123Test");
   await page.getByTestId('register-password-confirm').fill("1234Test");
   await page.getByTestId('register-submit').click();
-  // await page.pause();
+
   
   // Vérifie si le message d'erreur est présent
   await expect(page.getByText("Les mots de passe ne correspondent pas !")).toBeVisible();
