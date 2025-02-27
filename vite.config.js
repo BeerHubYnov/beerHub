d5a3c02a3ebd2c9d38779e4d4ffc72163736cfbb
Adel djahnit <adeldjahnita@gmail.com>
2025-02-27 15:50:20 +0100

    sentryOk

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "beerhub-6y",
+    project: "beerhub_sentry"
+  })],
+
   resolve: {
     alias: {
         '@react-three/fiber/jsx-runtime': 'react/jsx-runtime',
         '@react-three/fiber/jsx-dev-runtime': 'react/jsx-dev-runtime'
     }
+  },
+
+  build: {
+    sourcemap: true
   }
 })
