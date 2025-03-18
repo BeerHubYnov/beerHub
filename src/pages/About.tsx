40d666f402a070d96be888bf2a94d109fae3dcd3
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2025-03-18 11:15:43 +0100

    add about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,6 @@
 const About: React.FC = () => {
   return (
-    <div className="profil">
+    <div className="about">
       <h2>A propos</h2>
 
       <p>BeerHub est une application web dédiée à la gestion et à la découverte des bars et des événements associés. Elle offre une plateforme complète pour les utilisateurs et les propriétaires de bars, avec des fonctionnalités variées pour améliorer l'expérience utilisateur.</p>
